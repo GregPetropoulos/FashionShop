c6b42486cd33a95e4e20c91127e570a05400bad3
GP <gregpetropoulos@yahoo.com>
2023-05-31 14:52:16 -0400

    Implemented the LinkContainer from bootstrap in lieu of react-router-dom Link to prop

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,27 +2,36 @@ import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 // import logo from '../assets/logo.png'
-import logo from '../assets/T-logo.png'
+import logo from '../assets/T-logo.png';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>
-            <img src={logo} className={'logo-image'} alt='Tech Shop'/>
-            Tech Shop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img src={logo} className={'logo-image'} alt='Tech Shop' />
+              Tech Shop
+            </Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link hef='/cart'>
-                <FaShoppingCart />
-                Cart
-              </Nav.Link>
-              <Nav.Link hef='/login'>
-                <FaUser />
-                Sign In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <FaShoppingCart />
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <FaUser />
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
