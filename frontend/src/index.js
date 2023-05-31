580b23a76b90bf70285beff3ed2292ceaf6ef51e
GP <gregpetropoulos@yahoo.com>
2023-05-30 22:44:22 -0400

    Styles

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';//for just bootstrap
+import './assets/styles/index.css'; //custom styles
+import './assets/styles/bootstrap.custom.css'; //custom styles
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
