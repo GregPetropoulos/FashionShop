3133e54172c25dac1af45ce201502fe7e94a5e33
GP <gregpetropoulos@yahoo.com>
2023-06-08 22:01:19 -0400

    Added product.js mock file

---
+++ frontend/src/products.js
@@ -0,0 +1,38 @@
+export const products=[
+    {
+      name: 'test1',
+      _id: 1,
+      title: 'TEST1PRODUCTTITLE',
+      price: 20,
+      rating: 2,
+      numOfReviews: 12,
+      countInStock: 8,
+      description: 'loremloremlorem lorem loremlorem lorem',
+      image:
+        'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2899&q=80'
+    },
+    {
+      name: 'test2',
+      _id: 2,
+      title: 'TEST2PRODUCTTITLE',
+      price: 30,
+      rating: 5,
+      numOfReviews: 100,
+      countInStock: 10,
+      description: 'loremloremlorem lorem loremlorem lorem',
+      image:
+        'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2899&q=80'
+    },
+    {
+      name: 'test3',
+      _id: 3,
+      title: 'TEST3PRODUCTTITLE',
+      price: 40,
+      rating: 3.5,
+      numOfReviews: 27,
+      countInStock: 0,
+      description: 'loremloremlorem lorem loremlorem lorem',
+      image:
+        'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2899&q=80'
+    }
+  ];
