e6130190a01a562fe4b6eba8556b542f0cf9dac9
GP <gregpetropoulos@yahoo.com>
2023-06-08 22:00:41 -0400

    exporting mock file of products rather than hardcoding into the component

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,38 +1,8 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-const products = [
-  {
-    name: 'test1',
-    _id: 1,
-    title: 'TEST1PRODUCTTITLE',
-    price: 20,
-    rating: 2,
-    numOfReviews: 12,
-    image:
-      'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2899&q=80'
-  },
-  {
-    name: 'test2',
-    _id: 2,
-    title: 'TEST2PRODUCTTITLE',
-    price: 30,
-    rating: 5,
-    numOfReviews: 100,
-    image:
-      'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2899&q=80'
-  },
-  {
-    name: 'test3',
-    _id: 3,
-    title: 'TEST3PRODUCTTITLE',
-    price: 40,
-    rating: 3.5,
-    numOfReviews: 27,
-    image:
-      'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2899&q=80'
-  }
-];
+import { products } from '../products';
+
 const HomeScreen = () => {
   return (
     <>
