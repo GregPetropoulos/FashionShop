1e55a69eb2556d2f58c6347ddb96f504b3908c24
GP <gregpetropoulos@yahoo.com>
2023-07-05 17:24:37 -0400

    Check backend server works of hard coded mock data

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.get('/', (req, res) => {
@@ -18,4 +18,4 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(port, () => console.log(`Server running on ${port}`));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
