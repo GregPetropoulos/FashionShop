2fbf81dfd6ddb2245b9c074bc4ebb9f5f01c4b39
GP <gregpetropoulos@yahoo.com>
2023-07-09 00:37:48 -0400

    Implemented rtk and removed axios, useEffect and useState on the HomeScreen and ProductScreen

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,10 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
+// ======
+// export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
+// this will throw a cors error since we are using the proxy in package.json. If you want use this remove the proxy in the package.json
+// ======
+
+export const BASE_URL= ''
+
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
