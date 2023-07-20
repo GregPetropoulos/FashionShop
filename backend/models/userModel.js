6fd4f94180e6967cb1f1f3f5150232bf14a38d7c
GP <gregpetropoulos@yahoo.com>
2023-07-20 09:00:10 -0400

    Created matching password logic in userSchema to be used in login controller logic

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+import bcrypt from 'bcryptjs';
 const userSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
@@ -9,6 +9,10 @@ const userSchema = mongoose.Schema(
   },
   { timestamps: true }
 );
+// *Logic for matching plain text password at login against encrypted password in db in userController
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 export default User;
