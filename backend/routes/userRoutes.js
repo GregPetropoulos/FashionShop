c5b6408fa8274fca21a85c9840d7f01f386b9a4f
GP <gregpetropoulos@yahoo.com>
2023-07-21 00:03:49 -0400

    Implemented the middleware into each route as needed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,24 +11,27 @@ import {
   updateUser,
   deleteUsers,
 } from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 //routes are prefixed with /api/users/
 // if its a post request register the user and if it's get request get the users
-router.route('/').post(registerUser).get(getUsers);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 
 // Use router.post since there is only one post method needed
 router.post('/logout', logoutUser);
-router.post('/login', authUser);
+router.post('/auth', authUser);
 
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
-router.route('/:id').delete(deleteUsers).get(getUserById).put(updateUser)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUsers)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
-
-//*Admin Routes include:  
+//*Admin Routes include:
 // * getUsers,
 // * getUserById,
 // * updateUser,
 // * deleteUsers,
-
 
 export default router;
