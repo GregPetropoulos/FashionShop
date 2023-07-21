08187e490ca76ff38855e34c8d1624fe4234c232
GP <gregpetropoulos@yahoo.com>
2023-07-21 00:02:31 -0400

    Brought in the npm cookie-parser for middleware to access the cookie in the request

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,32 @@ import express from 'express';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import colors from 'colors';
 dotenv.config();
 import connectDB from './config/db.js';
 
 const PORT = process.env.PORT || 5000;
-connectDB(); // Connect to MongoDB
+
+//* CONNECT MONGODB
+connectDB();
+
 const app = express();
-// request Body Parser middleware
+
+// * REQUEST BODY PARSER MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//* COOKIE PARSER MIDDLEWARE--allows us to access request.cookies
+app.use(cookieParser());
+
+//* TEST ROUTE
 // app.get('/', (req, res) => {
 //   res.send('API IS RUNNING');
 // });
 
+// * ROUTES
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use(notFound);
