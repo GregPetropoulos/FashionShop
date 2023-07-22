35af788a22a07634df4be876cfd15543a26b3d29
GP <gregpetropoulos@yahoo.com>
2023-07-21 21:24:19 -0400

    Register user logic to create a new user in the user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
-import jwt from 'jsonwebtoken';
+import generateToken from '../utils/generateToken.js';
 
 // @desc    Auth the user and get token
 // @route   POST /api/users/login
@@ -9,16 +9,7 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
-
-    //* SET JWT AS HTTP-only cookie
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development', //if not in dev env then false, use http in prod use https
-      sameSite: 'strict', //prevent attacks
-      maxAge: 30 * 24 * 60 * 1000, //30 days
-    });
-
+    generateToken(res, user._id);
     res.json({
       _id: user._id,
       name: user.name,
@@ -35,7 +26,29 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  PUBLIC
 const registerUser = asyncHandler(async (req, res) => {
-  res.send('register user');
+  const { email, name, password } = req.body;
+
+  //CHECK DB IF USER ALREADY EXISTS
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  //CREATING A NEW USER
+  const user = await User.create({ name, email, password });
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 // @desc    Logout user / clear cookie
