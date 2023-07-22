b421e230c2ba51e3e7ae477d230ddcf4c226df07
GP <gregpetropoulos@yahoo.com>
2023-07-22 13:59:17 -0400

    Created a auth slice to set the user's creds into local storage and check local storage for existing user auth

---
+++ frontend/src/slices/authSlice.js
@@ -0,0 +1,21 @@
+// Setting user credentials and saving to local storage, no api points needed
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItems('userInfo')) : null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      // By hitting the backend via userApi slice we get this payload
+      state.userInfo = action.payload;
+      localStorage.setItem('userInfo', JSON.stringify(action.payload));
+    },
+  },
+});
+export const { setCredentials } = authSlice.actions;
+export default authSlice.reducer;
