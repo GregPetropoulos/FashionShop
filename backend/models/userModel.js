9eea0af8df4a1bdb75928fdbca5433a120aab195
GP <gregpetropoulos@yahoo.com>
2023-07-21 21:22:33 -0400

    Encrypt password of a new registered user before it's sent to db

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -14,5 +14,15 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+//* ENCRYPT PASSWORD BEFORE IT'S SENT TO DB BY USING THE .pre
+userSchema.pre('save', async function (next) {
+  //if not changing the user's password then move on
+  if (!this.isModified('password')) {
+    next();
+  }
+  //if password is changed then hash it
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 const User = mongoose.model('User', userSchema);
 export default User;
