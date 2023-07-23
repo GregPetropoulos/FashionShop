1cf404c52c287cbc8bae4125a75d9ecadaf53e67
GP <gregpetropoulos@yahoo.com>
2023-07-23 14:58:23 -0400

    reducer to save shipping address, and added the shipping object to initial state aling with Paypal key,value

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -4,9 +4,10 @@ import { createSlice } from '@reduxjs/toolkit';
 import { updateCart } from '../utils/cartUtils';
 
 //So the order keeps items in cart
+//Icebox goal - Integrate stripe
 const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
-  : { cartItems: [] };
+  : { cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' };
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -25,12 +26,16 @@ const cartSlice = createSlice({
     },
     removeFromCart: (state, action) => {
       state.cartItems = state.cartItems.filter((item) => item._id !== action.payload);
-      return updateCart(state)
+      return updateCart(state);
+    },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+      return updateCart(state);
     },
   },
 });
 // * ANY REDUCER FUNCTION IN THE REDUCERS{} MUST BE EXPORTED
 
-export const { addToCart,removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress } = cartSlice.actions;
 
 export default cartSlice.reducer;
