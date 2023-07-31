f2d0e53ec2d102a9f8ef3d4e91a6f1761549fa00
GP <gregpetropoulos@yahoo.com>
2023-07-30 23:50:27 -0400

    Add route for ProductListScreen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -27,7 +27,7 @@ import OrderScreen from './screens/OrderScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import AdminRoute from './components/AdminRoute';
 import { OrderListScreen } from './screens/admin/OrderListScreen';
-
+import ProductListScreen from './screens/admin/ProductListScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -47,7 +47,8 @@ const router = createBrowserRouter(
         <Route path='/profile' element={<ProfileScreen />} />
       </Route>
       <Route path='' element={<AdminRoute />}>
-        <Route path='/admin/orderlist' element={<OrderListScreen/>} />
+        <Route path='/admin/orderlist' element={<OrderListScreen />} />
+        <Route path='/admin/productlist' element={<ProductListScreen />} />
       </Route>
     </Route>
   )
