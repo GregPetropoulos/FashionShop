05870a6f57c67b389acf5015369f8d94b2d8a05c
GP <gregpetropoulos@yahoo.com>
2023-07-30 23:49:19 -0400

    Two admin backend routes which are the updateOrderToDelivered, getOrders  in the order controller logic

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -91,14 +91,25 @@ const getOrderById = asyncHandler(async (req, res) => {
 // @route   GET /api/orders/:id/deliver
 // @access  PRIVATE/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
-  res.send('update order delivered, admin action');
+  const order = await Order.findById(req.params.id);
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+    const updateOrder = await order.save();
+    res.status(200).json(updateOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
 });
 
 // @desc    Get all orders
 // @route   GET /api/orders
 // @access  PRIVATE/Admin
 const getOrders = asyncHandler(async (req, res) => {
-  res.send('get all orders, admin action');
+  // Find all orders, populate from the user collection the id and name
+  const orders = await Order.find({}).populate('user', 'id name');
+  res.status(200).json(orders);
 });
 //* ADMIN
 // *======================
