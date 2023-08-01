cf0b216ad65f1d8f1f167988447a66ab97b26393
GP <gregpetropoulos@yahoo.com>
2023-08-01 00:50:29 -0400

    Backend controller logic and routes for updating a product vua admin user

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -32,18 +32,43 @@ const getProductById = asyncHandler(async (req, res) => {
 // @route   POST /api/products
 // @access  PRIVATE/ADMIN
 const createProducts = asyncHandler(async (req, res) => {
-const product = new Product({
-  name:'Sample Name',
-  price:0,
-  user:req.user._id,
-  image:'/images/sample.jpg',
-  brand:'Sample Brand',
-  category:'Sample Category',
-  countInStock:0,
-  numReviews:0,
-  description:'Sample Description lorem  lorem'
-})
-const createProduct = await product.save();
-res.status(201).json(createProduct)
+  const product = new Product({
+    name: 'Sample Name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample Brand',
+    category: 'Sample Category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample Description lorem  lorem',
+  });
+  const createProduct = await product.save();
+  res.status(201).json(createProduct);
 });
-export { getProductById, getProducts,createProducts };
+
+// @desc    Update product
+// @route   PUT /api/products/:id
+// @access  PRIVATE/ADMIN
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } = req.body;
+  const product = await Product.findById(req.params.id);
+  console.log(req.params._id)
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+    const updatedProduct = await product.save();
+    res.json(updatedProduct)
+  }else{
+    res.status(404);
+    throw new Error('Resource not found')
+  }
+});
+
+export { getProductById, getProducts, createProducts,updateProduct };
