6b977fe4cc6160dc6e5850280f672d2597b9e541
GP <gregpetropoulos@yahoo.com>
2023-08-01 00:52:12 -0400

    Post call to mutation to backend to create with a template response and update with PUT request

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -7,6 +7,7 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       query: () => ({
         url: PRODUCTS_URL,
       }),
+      providesTags: ['Products'],
       keepUnusedDataFor: 5, //caching
     }),
     getProductDetails: builder.query({
@@ -15,14 +16,27 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       }),
     }),
     createProduct: builder.mutation({
-      query:()=>({
-        //Not passing data because we have a new sample created on the back endthat we can edit
-        url:PRODUCTS_URL,
-        method:'POST'
+      query: () => ({
+        //Not passing data because we have a new sample created on the back end that we can edit
+        url: PRODUCTS_URL,
+        method: 'POST',
       }),
-      invalidatesTags:['Product'],//Stops from being cached so we have fresh data
-    })
+      invalidatesTags: ['Product'], //Stops from being cached so we have fresh data
+    }),
+    updateProduct: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: ['Products'],
+    }),
   }),
 });
 
-export const { useGetProductsQuery, useGetProductDetailsQuery,useCreateProductMutation } = productsApiSlice;
+export const {
+  useGetProductsQuery,
+  useGetProductDetailsQuery,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+} = productsApiSlice;
