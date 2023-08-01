054cd881539c9a5ea672bdce27a5c7d84bd448a1
GP <gregpetropoulos@yahoo.com>
2023-07-31 22:46:51 -0400

    Routed router

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { getProducts, getProductById } from '../controllers/productController.js';
+import { getProducts, getProductById,createProducts } from '../controllers/productController.js';
+import {protect, admin} from '../middleware/authMiddleware.js'
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect,admin,createProducts);
 
 router.route('/:id').get(getProductById);
+
 
 export default router;
