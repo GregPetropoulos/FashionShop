fee814b77b0410958ec8637f454322547afcf5a4
GP <gregpetropoulos@yahoo.com>
2023-08-01 23:07:07 -0400

    Server routes for and created a static upload folder for front end

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+import path from 'path';
 import express from 'express';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
@@ -32,8 +34,17 @@ app.use(cookieParser());
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+
+//* UPLOAD ROUTE
+app.use('/api/upload', uploadRoutes);
+
 // *PAYPAL-ROUTE
 app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }));
+
+//*SET THE UPLOAD FOLDER THAT IMAGES ARE BEING UPLOADED TO AS STATIC FOLDER
+const __dirname = path.resolve(); //set dirname to current folder
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
 app.use(notFound);
 app.use(errorHandler);
 
