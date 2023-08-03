9c7d918a5a0cc44368d036f029e3d1942ab24779
GP <gregpetropoulos@yahoo.com>
2023-08-02 23:46:22 -0400

    Delete product mutation basically sending the product id and DELETE method over the wire for the backend to handle

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,4 +1,4 @@
-import { PRODUCTS_URL,UPLOAD_URL } from '../constants';
+import { PRODUCTS_URL, UPLOAD_URL } from '../constants';
 import { apiSlice } from './apiSlice';
 
 export const productsApiSlice = apiSlice.injectEndpoints({
@@ -32,12 +32,18 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       invalidatesTags: ['Products'],
     }),
     uploadProductImage: builder.mutation({
-      query:(data)=>({
-        url:`${UPLOAD_URL}`,
-        method:'POST',
-        body:data
-      })
-    })
+      query: (data) => ({
+        url: `${UPLOAD_URL}`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    deleteProduct: builder.mutation({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+        method: 'DELETE',
+      }),
+    }),
   }),
 });
 
@@ -46,5 +52,6 @@ export const {
   useGetProductDetailsQuery,
   useCreateProductMutation,
   useUpdateProductMutation,
-  useUploadProductImageMutation
+  useUploadProductImageMutation,
+  useDeleteProductMutation
 } = productsApiSlice;
