bdea85b0ebd43e9e030f16816fcbb7f5ebbc69b5
GP <gregpetropoulos@yahoo.com>
2023-08-09 00:31:45 -0400

    Carousel.Caption className styles override via index.css file

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -14,7 +14,7 @@ const ProductCarousel = () => {
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
-            <Carousel.Caption >
+            <Carousel.Caption className='carousel-caption' >
               <h2 className='text-white text-right'>
                 {product.name} (${product.price})
               </h2>
