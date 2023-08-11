6bbd5155240ffde4e2b20f058d39e989eaf1b378
GP <gregpetropoulos@yahoo.com>
2023-08-11 00:39:09 -0400

    Seeded db,link up github to local git

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,71 +1,105 @@
 const products = [
   {
-    name: 'Airpods Wireless Bluetooth Headphones',
-    image: '/images/airpods.jpg',
+    name: 'Rhinestone Cross Choker Necklace',
+    image: '/images/necklace-1.jpeg',
     description:
-      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
-    brand: 'Apple',
-    category: 'Electronics',
-    price: 89.99,
+      'A choker necklace featuring a cross pendant with allover rhinestone embellishments, Chain length: 70".',
+    brand: 'Forever 21',
+    category: 'Jewelry',
+    price: 9.99,
     countInStock: 10,
     rating: 4.5,
     numReviews: 12,
   },
   {
-    name: 'iPhone 11 Pro 256GB Memory',
-    image: '/images/phone.jpg',
+    name: 'Mid Rise Baby Boot Jeans with Washwell',
+    image: '/images/womens-bottoms-1.webp',
     description:
-      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
-    brand: 'Apple',
-    category: 'Electronics',
-    price: 599.99,
+      `A classic bootcut jean with a mini-gone-major update. We kept it straight through the leg — then ended with a slight flare. This is the 2.0 version of a forever favorite.
+
+      - Fit: A full-length slim fit with a mini bootcut leg.​
+      - Fabric: 81% Cotton, 4% Recycled Materials, 6% Stretch.
+      - Stretch: High Stretch Jeans. Innovative stretch with high recovery. ​Made to move & always bounces back.​
+      - Rise: Mid Rise Jeans.
+      - Look: A classic five-pocket jean in a medium wash.
+      - Details: Zip fly & five-pocket styling.
+      Responsibly Made: This pair of jeans is part of our water-saving Washwell program. Compared to conventional wash methods, Washwell has saved millions of liters of water since 2016.`,
+    brand: 'GAP',
+    category: 'Jeans',
+    price: 89.99,
     countInStock: 7,
     rating: 4.0,
     numReviews: 8,
   },
   {
-    name: 'Cannon EOS 80D DSLR Camera',
-    image: '/images/camera.jpg',
-    description:
-      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
-    brand: 'Cannon',
-    category: 'Electronics',
-    price: 929.99,
+    name: 'Ruffle-Trim Maxi Dress',
+    image: '/images/womens-dress-1.jpg',
+    description:`A woven maxi dress featuring a smocked bodice, square-cut neckline and back, shoulder straps with ruffle trim, a column silhouette, and flounce hem.
+    - This is an independent brand and not a Forever 21 branded item.
+
+    Content + Care
+    - 100% polyester rayon
+    
+    - Machine wash cold
+    
+    Size + Fit
+    - Model is 5'7" and wearing a Small`,
+    brand: 'Independent',
+    category: 'Dress',
+    price: 48.99,
     countInStock: 5,
     rating: 3,
     numReviews: 12,
   },
   {
-    name: 'Sony Playstation 4 Pro White Version',
-    image: '/images/playstation.jpg',
-    description:
-      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
-    brand: 'Sony',
-    category: 'Electronics',
-    price: 399.99,
+    name: 'Plunging Cutout Maxi Dress',
+    image: '/images/womens-dress-2.webp',
+    description:`A woven maxi dress featuring a plunging neckline, shoulder straps, low-cut back, front and back cutouts, M-slit, and column silhouette.
+    Content + Care
+    - 100% rayon
+    
+    - Hand wash cold
+    
+    Size + Fit
+    - Model is 5'7" and wearing a Small`,
+    brand: 'Forever21',
+    category: 'Dress',
+    price: 24.99,
     countInStock: 11,
     rating: 5,
     numReviews: 12,
   },
   {
-    name: 'Logitech G-Series Gaming Mouse',
-    image: '/images/mouse.jpg',
-    description:
-      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
-    brand: 'Logitech',
-    category: 'Electronics',
-    price: 49.99,
+    name: 'Collared Zip-Up Mini Dress',
+    image: '/images/womens-dress-3.webp',
+    description:`A canvas mini dress featuring a zip-front closure, plunging V-neckline, basic collar, seam detailing, cap sleeves, pleating, and a fit and flare silhouette.
+    Content + Care
+    - 100% cotton
+    
+    - Machine wash cold
+    
+    Size + Fit
+    - Model is 5'7" and wearing a Small`,
+    brand: 'Forever21',
+    category: 'Dress',
+    price: 29.99,
     countInStock: 7,
     rating: 3.5,
     numReviews: 10,
   },
   {
-    name: 'Amazon Echo Dot 3rd Generation',
-    image: '/images/alexa.jpg',
-    description:
-      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
-    brand: 'Amazon',
-    category: 'Electronics',
+    name: 'Contour Cropped Tube Top',
+    image: '/images/womens-top-1.jpeg',
+    description:`This must-have shapewear tube top flatters, lifts, and smooths without constricting! It features a cropped hem, straight-cut neckline and back, as well as a lightweight double-layered knit fabric that contours the body for a flattering and smoothing fit with minimal seams and high functionality.
+    Content + Care
+    - 90% nylon, 10% spandex
+    
+    - Hand wash cold
+    
+    Size + Fit
+    - Model is 5'8" and wearing a S/M`,
+    brand: 'Forever21',
+    category: 'Tops',
     price: 29.99,
     countInStock: 0,
     rating: 4,
