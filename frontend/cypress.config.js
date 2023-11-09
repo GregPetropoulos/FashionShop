c1c1decd62b6cad6dd4bbb5b16ac169001f23a24
GP <gregpetropoulos@yahoo.com>
2023-11-09 09:45:22 -0500

    Configuration added to e2e test and several refactors to test

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -10,9 +10,10 @@ module.exports = defineConfig({
   },
 
   e2e: {
-    // baseUrl:'https://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl:'http://localhost:3000',
   },
 });
+
--- frontend/cypress/e2e/spec.cy.js
+++ frontend/cypress/e2e/spec.cy.js
@@ -1,19 +1,36 @@
 /* global cy */
-
-const paginationItems = ['1', '2', '3'];
+/// <reference types="Cypress" />
+// const paginationItems = ['1', '2', '3'];
 describe('Initial Load of Fashion Shop App', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000');
-    // Nav Link
-    // Search
-    // Carousel
-    // Products
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
-    
-    // Pagination
+  it('passes', () => {
+    // Nav Link
+    cy.get('header .navbar .container a').should('have.attr', 'href', '/');
+  });
+
+  // Nav Link
+  it('Navbar Passes', () => {
+    cy.get('header .navbar .container a').should('have.attr', 'href', '/');
+  });
+
+  // Search
+  it('Search accepts inputs', () => {
+    const typedInput = 'jeans';
+    cy.focused().should('have.class', 'form-control');
+    cy.get('.form-control').type(typedInput).should('have.value', typedInput);
+  });
+
+  // Carousel
+
+  // Products
+
+  // Pagination
+  it('Paginated Links', () => {
     cy.get('.pagination li').contains('1').should('have.attr', 'href', '/page/1');
     cy.get('.pagination li').contains('2').should('have.attr', 'href', '/page/2');
     cy.get('.pagination li ').contains('3').should('have.attr', 'href', '/page/3');
   });
-  });
-
+});
