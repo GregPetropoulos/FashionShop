b75ddf771c067ae2b1152fd59e7022e6a3428cb7
GP <gregpetropoulos@yahoo.com>
2023-11-11 16:26:12 -0500

    Prettier should fail

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import { ToastContainer } from 'react-toastify';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
